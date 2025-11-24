fa1e64d305c43bc983379c3e408de0c42bb9ba9d
Sourabh Choudhary <csourabh003@gmail.com>
2025-11-24 19:51:59 +0530

    javascript changs

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+/** Template Strings */
+// console.log(`Hi ${firstName} ${lastName}`);
+
+/** Operators */
+
+// + contactiante, plus
+// - minus
+// / divide
+// * multiply
+// % remainder (modulo)
+// ** exponentiation
+
+/** Types */
+
+// let firstName = null; // object
+// let lastName; // undefined
+// let age = 25;
+// let isMarried = false;
+// let salary = 45000.50;
+// let grade = 'A';
+
+// undefined
+// null (object)
+
+/** Functions */
+// 2 stages
+
+// function declaration
+function square(num1) {
+	return num1 ** 2;
+
+	console.log("This will not be printed");
+}
+
+// function call / invoke
+// console.log(square(8));
+
+// inbuilt functions
+
+// alert('This is an error');
+// var fullNafme = prompt('Enter your name');
+// var val = confirm('Are you sure you want to delete this file?');
+
+// console.log(val);
