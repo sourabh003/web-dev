bf97db30e7a184a98bc02727bb0a06b33958df77
Sourabh Choudhary <csourabh003@gmail.com>
2025-11-27 22:05:20 +0530

    added loops

---
+++ scripts/loops.js
@@ -0,0 +1,45 @@
+// for
+// for...in
+// for...of
+
+// while
+
+// do while
+
+// increment operator ++, plus 1
+// decrement operator --, minus 1
+
+//        num += count, add then assign
+//        num -= count, minus then assign
+//        num *= count, multiply then assign
+//        num /= count, divide then assign
+
+// prefix, ++count
+// postfix, count++
+
+function runLoop() {
+	// let count = 10;
+
+	// while (count < 10) {
+	//     console.log('while loop count: ' + count);
+	// }
+
+	// do {
+	//     console.log('do while loop count: ' + count);
+	//     count++
+    // } while (count < 10)
+    
+    for (let count = 0; count <10; count++) {
+        console.log(count)
+    }
+
+    // for (let outer = 1; outer <= 5; outer++) {
+    //     let str = ""
+	// 	for (let inner = 1; inner <= outer; inner++) {
+    //         str += "*"
+    //     }
+    //     console.log(str);
+	// }
+}
+
+// runLoop(); 
